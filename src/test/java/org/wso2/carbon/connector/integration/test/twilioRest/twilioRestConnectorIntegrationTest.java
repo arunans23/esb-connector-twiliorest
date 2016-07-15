82b6af5097d3a90a399c4242aac6a9e6e5790715
Rajjaz <hmrajas1990@gmail.com>
2016-07-15 14:21:29 +0530

    Integration test version added

--- src/test/java/org/wso2/carbon/connector/integration/test/twilioRest/twilioRestConnectorIntegrationTest.java
+++ src/test/java/org/wso2/carbon/connector/integration/test/twilioRest/twilioRestConnectorIntegrationTest.java
@@ -43,7 +43,7 @@ public class twilioRestConnectorIntegrationTest extends ConnectorIntegrationTest
     @BeforeClass(alwaysRun = true)
     public void setEnvironment() throws Exception {
 
-        init("twilioRest-connector-1.0.1-SNAPSHOT");
+        init("twilioRest-connector-1.0.1");
         esbRequestHeadersMap.put("Accept-Charset", "UTF-8");
         esbRequestHeadersMap.put("Content-Type", "application/json");
         apiRequestHeadersMap.put("Content-Type", "application/x-www-form-urlencoded");
